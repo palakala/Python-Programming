258de742955674f529a35f98cf62a274e255b406
Palakala Sai Krishna Yadhav <60471421+palakala@users.noreply.github.com>
2020-10-12 13:33:24 +0530

    Add files via upload

---
+++ Arrays/howmanysmall1.py
@@ -0,0 +1,7 @@
+class Solution:
+    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        count = {}
+        for i, v in enumerate(sorted(nums)):
+            if v not in count:
+                count[v] = i
+        return [count[v] for v in nums]
