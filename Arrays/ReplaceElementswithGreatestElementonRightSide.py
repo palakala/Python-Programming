9aabded3c9cf92d1d82442210f6d3006de4abfbd
Palakala Sai Krishna Yadhav <60471421+palakala@users.noreply.github.com>
2020-11-12 19:11:53 +0530

    Update ReplaceElementswithGreatestElementonRightSide.py

--- Arrays/ReplaceElementswithGreatestElementonRightSide.py
+++ Arrays/ReplaceElementswithGreatestElementonRightSide.py
@@ -14,7 +14,7 @@ class Solution:
 
 Given an array arr, replace every element in that array with the greatest element among the elements to its right, and replace the last element with -1.
 
-After doing so, return the array.
+After doing so, return the array .
 
 Example 1:
 
